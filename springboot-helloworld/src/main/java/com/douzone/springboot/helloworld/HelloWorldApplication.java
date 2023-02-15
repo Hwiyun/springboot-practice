40543ba613b1eafc831f5dd5056c275debbc93a9
Hwiyun <sarakain123@gmail.com>
2023-02-15 15:58:28 +0900

    Springboot - MyApplication #2

--- springboot-helloworld/src/main/java/com/douzone/springboot/helloworld/HelloWorldApplication.java
+++ springboot-helloworld/src/main/java/com/douzone/springboot/helloworld/HelloWorldApplication.java
@@ -16,11 +16,14 @@ public class HelloWorldApplication {
 		/**
 		 * SpringApplication.run(...) 안에서 일어나는 일(Bootstrap)
 		 * 
-		 * 1. 어플리케이션 컨텍스트(Application Context, Spring Container) 생성 2. 만약, 웹 어플리케이션이면, 웹
-		 * 어플리케이션 타입을 결정(SpringMVC, or Reactive) 3. 어노테이션 스캐닝(Auto) + Configuration
-		 * Class를 통한 빈 생성/등록/와이어링 작업 4. 만약 웹 어플리케이션이고 타입이 Spring MVC이면 - 내장(embeded)
-		 * 서버(TomcatEmbededServiceServletContainer) 인스턴스 생성 - 서버 인스턴스에 웹 어플리케이션 배포 - 서버
-		 * 인스턴스 실행 5. ApplicationRunner 인터페이스를 구현한 빈을 Aplication Context에서 찾아서 실행
+		 * 1. 어플리케이션 컨텍스트(Application Context, Spring Container) 생성 
+		 * 2. 만약, 웹 어플리케이션이면, 웹 어플리케이션 타입을 결정(SpringMVC, or Reactive) 
+		 * 3. 어노테이션 스캐닝(Auto) + Configuration Class를 통한 빈 생성/등록/와이어링 작업 
+		 * 4. 만약 웹 어플리케이션이고 타입이 Spring MVC이면 
+		 * - 내장(embeded) 서버(TomcatEmbededServiceServletContainer) 인스턴스 생성 
+		 * - 서버 인스턴스에 웹 어플리케이션 배포 
+		 * - 서버 인스턴스 실행 
+		 * 5. ApplicationRunner 인터페이스를 구현한 빈을 Aplication Context에서 찾아서 실행
 		 */
 
 //		ConfigurableApplicationContext ac = null;
@@ -36,8 +39,6 @@ public class HelloWorldApplication {
 //		}
 
 		// try ~ with ~ resources 구문
-		try (ConfigurableApplicationContext ac = SpringApplication.run(HelloWorldApplication.class, args)) {
-
-		}
+		try (ConfigurableApplicationContext ac = SpringApplication.run(HelloWorldApplication.class, args)){}
 	}
 }
